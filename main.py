6cf91211c56cc51eeb91e171d1d8d0d56f179dc5
Marcell-Davis <kyr3xxstudios@gmail.com>
2026-01-16 22:06:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Module
+import math
 from datetime import date
 import os
 import time
@@ -559,3 +560,42 @@ while True:
             time.sleep(1)
             print("ERR_KERNEL_PANIC")
             os.system('shutdown -f' if os.name == 'nt' else 'systemctl poweroff')
+
+        if action == "starten:calc":
+            print(" CCC    A   L      CCC  U   U L       A   TTTTT  OOO  RRRR ")
+            print("C   C  A A  L     C   C U   U L      A A    T   O   O R   R")
+            print("C     A   A L     C     U   U L     A   A   T   O   O R   R")
+            print("C     A   A L     C     U   U L     A   A   T   O   O RRRR ")
+            print("C     AAAAA L     C     U   U L     AAAAA   T   O   O R R  ")
+            print("C   C A   A L     C   C U   U L     A   A   T   O   O R  R ")
+            print(" CCC  A   A LLLLL  CCC   UUU  LLLLL A   A   T    OOO  R   R")
+            print("-----------------------------------------------------------")
+            print("MarcellD'Avis Calculator                              V1.0R")
+            print("")
+            firstnum = input("Erste Zahl: ")
+            secondnum = input("Zweite Zahl: ")
+            operation = input("Operation ((A)ddieren, (S)ubtrahieren, (M)ultiplizieren, (D)ividieren): ")
+            if operation == "A":
+                result = float(firstnum) + float(secondnum)
+                if result == 67:
+                    print("Hör auf, ein 9-jähriger zu sein. Arschloch. (hol dir trotzdem 1&1 produkte)")
+                else:
+                    print("Das Ergebnis ist 1&1. (Spaß, es ist " + str(result) + ")")
+            if operation == "S":
+                result = float(firstnum) - float(secondnum)
+                if result == 67:
+                    print("Hör auf, ein 9-jähriger zu sein. Arschloch. (hol dir trotzdem 1&1 produkte)")
+                else:
+                    print("Das Ergebnis ist 1&1. (Spaß, es ist " + str(result) + ")")
+            if operation == "M":
+                result = float(firstnum) * float(secondnum)
+                if result == 67:
+                    print("Hör auf, ein 9-jähriger zu sein. Arschloch. (hol dir trotzdem 1&1 produkte)")
+                else:
+                    print("Das Ergebnis ist 1&1. (Spaß, es ist " + str(result) + ")")
+            if operation == "D":
+                result = float(firstnum) / float(secondnum)
+                if result == 67:
+                    print("Hör auf, ein 9-jähriger zu sein. Arschloch. (hol dir trotzdem 1&1 produkte)")
+                else:
+                    print("Das Ergebnis ist 1&1. (Spaß, es ist " + str(result) + ")")
